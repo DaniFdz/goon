cd728c3bcb447ce9489bb045cbf971f2153010fe
DaniFdz <danifernandezzzzzz@gmail.com>
2023-12-23 13:37:05 +0100

    Added logging formaters

--- core/project/settings/templates/settings.dev.py
+++ core/project/settings/templates/settings.dev.py
@@ -1,2 +1,10 @@
 DEBUG = True
 SECRET_KEY = "django-insecure-3)gqd#y5inj052qqcsdt%_*$+ol-xc1onl4^071v=2=_lr587)"
+
+LOGGING["formatters"]["colored"] = {  # type: ignore # noqa: F821
+    "()": "colorlog.ColoredFormatter",
+    "format": "%(log_color)s%(asctime)s %(levelname)s %(name)s %(bold_white)s%(message)s",
+}
+LOGGING["loggers"]["cooking_core"]["level"] = "DEBUG"  # type: ignore # noqa: F821
+LOGGING["handlers"]["console"]["level"] = "DEBUG"  # type: ignore # noqa: F821
+LOGGING["handlers"]["console"]["formatter"] = "colored"  # type: ignore # noqa: F821
