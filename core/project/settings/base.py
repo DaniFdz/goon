6299e9d669603829cd89f1ba40d25106556ea80f
DaniFdz <danifernandezzzzzz@gmail.com>
2023-12-25 18:29:48 +0100

    Production static files solved

--- core/project/settings/base.py
+++ core/project/settings/base.py
@@ -91,10 +91,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/5.0/howto/static-files/
 
 MEDIA_URL = '/media/'
-MEDIA_ROOT = 'media'  # type: ignore # noqa: F821
+MEDIA_ROOT = BASE_DIR / 'media'
 
 STATIC_URL = '/static/'
-STATIC_ROOT = 'static'  # type: ignore # noqa: F821
+STATIC_ROOT = BASE_DIR / 'static'
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
