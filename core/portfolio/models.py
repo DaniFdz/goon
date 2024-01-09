830df04afe7fe46eba35f7cc5d1df3e12a891678
DaniFdz <danifernandezzzzzz@gmail.com>
2024-01-09 21:50:26 +0100

    Added max size 50MB to image validations

--- core/portfolio/models.py
+++ core/portfolio/models.py
@@ -1,3 +1,4 @@
+from django.core.exceptions import ValidationError
 from django.db import models
 
 
@@ -9,8 +10,15 @@ class Categorie(models.Model):
         return self.name
 
 
+def validate_image(fieldfile_obj):
+    filesize = fieldfile_obj.file.size
+    megabyte_limit = 50.0
+    if filesize > megabyte_limit * 1024 * 1024:
+        raise ValidationError("Max file size is %sMB" % str(megabyte_limit))
+
+
 class Clothe(models.Model):
-    image = models.ImageField(upload_to="images/")
+    image = models.ImageField(upload_to="images/", validators=[validate_image])
     name = models.CharField(max_length=50, null=True)
     description = models.CharField(max_length=1000)
     category = models.ForeignKey(Categorie, on_delete=models.CASCADE, null=True)
