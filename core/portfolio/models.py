34af504c21887f42c28d837f799f77dd245d9bf5
DaniFdz <danifernandezzzzzz@gmail.com>
2024-01-13 15:50:34 +0100

    Limited images size

--- core/portfolio/models.py
+++ core/portfolio/models.py
@@ -1,4 +1,7 @@
+import os
+
 from django.db import models
+from PIL import Image
 
 
 class Categories(models.Model):
@@ -14,6 +17,22 @@ class Clothes(models.Model):
     name = models.CharField(max_length=50, null=True)
     description = models.CharField(max_length=1000)
     category = models.ForeignKey(Categories, on_delete=models.CASCADE, null=True)
+
+    def save(self, *args, **kwargs):
+        super().save(*args, **kwargs)
+
+        # Set the maximum size in bytes (5MB)
+        max_size_bytes = 1 * 1024 * 1024
+        # Open the uploaded image file
+        img = Image.open(self.image.path)
+
+        # Check if the image is larger than the max size
+        quality = 100
+        while os.path.getsize(self.image.path) > max_size_bytes and quality > 0:
+            # Reduce the quality
+            img.save(self.image.path, quality=quality)
+
+            quality -= 1
 
     def __str__(self):
         if self.category:
