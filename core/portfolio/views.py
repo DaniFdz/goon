837bff7d71c4917de5a8d2d0509644a8a346c606
DaniFdz <danifernandezzzzzz@gmail.com>
2024-01-20 15:20:47 +0000

    Portfolio not responsive

--- core/portfolio/views.py
+++ core/portfolio/views.py
@@ -1,5 +1,17 @@
+from typing import Dict
+
 from django.shortcuts import render
+
+from .models import Categorie, Clothe
 
 
 def portfolio(request):
-    return render(request, "pages/portfolio.html", {})
+    clothes_ordered = Clothe.objects.all()
+
+    context: Dict[str, Dict[str, Clothe | Categorie]] = {}
+    for clothe in clothes_ordered:
+        if clothe.category.name not in context:
+            context[clothe.category.name] = {"category": clothe.category, "clothes": []}
+        context[clothe.category.name]["clothes"].append(clothe)
+
+    return render(request, "pages/portfolio.html", {"categories": context.values()})
